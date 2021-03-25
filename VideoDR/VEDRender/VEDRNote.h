4b2fa79d9d5ae8ea49a3526004f33d7cb4c4671c
yanzhen <sdyzsdut@163.com>
2021-03-25 17:31:54 +0800

    VEDNote E&D

---
+++ VideoDR/VEDRender/VEDRNote.h
@@ -0,0 +1,16 @@
+//
+//  VEDRNote.h
+//  VideoDR
+//
+//  Created by yanzhen on 2021/3/25.
+//
+
+/** iPhone6s 640x480
+ 采集kCVPixelFormatType_420YpCbCr8BiPlanarFullRange
+ 
+ 不渲染：
+    解码
+    kCVPixelFormatType_32BGRA                      3% 21MB
+    kCVPixelFormatType_420YpCbCr8BiPlanarFullRange 3% 20.9MB
+    kCVPixelFormatType_420YpCbCr8Planar            3% 21MB
+ */
